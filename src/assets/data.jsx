94bd6c8e40076db623362a45856538dcf0110bc4
anjakely18 <anjaniaina03@gmail.com>
2025-02-13 22:58:07 +0100

    mise a jour

--- src/assets/data.jsx
+++ src/assets/data.jsx
@@ -1,9 +1,16 @@
 import { nanoid } from "nanoid";
-import { FaHtml5, FaJs, FaReact, FaPhp } from "react-icons/fa";
-import cocktail from "./cocktail-verse.png";
-import toDo from "./to-do-list.png";
-import simon from "./simon-game.png";
-import blog from "./blog.png";
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaFigma } from "react-icons/fa";
+import { MdCampaign } from "react-icons/md";
+import {
+  SiAdobeillustrator,
+  SiBootstrap,
+  SiTailwindcss,
+  SiSass,
+  SiPhp,
+  SiMysql,
+} from "react-icons/si";
+import cuej from "./cuej.png";
+import sobriete from "./sae.png";
 
 export const links = [
   { id: nanoid(), href: "#home", text: "Accueil" },
@@ -15,9 +22,9 @@ export const links = [
 export const skills = [
   {
     id: nanoid(),
-    title: "HTML&CSS",
+    title: "HTML & CSS",
     icon: <FaHtml5 className="skill-icon" />,
-    text: "Maîtrise de la structure et du style des pages web pour créer des interfaces utilisateur attrayantes et fonctionnelles.",
+    text: "Maîtrise des bases du développement web avec HTML et CSS. Expérience avec Bootstrap, SCSS et Tailwind pour styliser efficacement les interfaces.",
   },
   {
     id: nanoid(),
@@ -33,43 +40,45 @@ export const skills = [
   },
   {
     id: nanoid(),
-    title: "PHP",
-    icon: <FaPhp className="skill-icon" />,
-    text: "Connaissance en PHP pour gérer la logique côté serveur et l'interaction avec les bases de données.",
+    title: "PHP & Back-end",
+    icon: <SiPhp className="skill-icon" />,
+    text: "Expérience en développement back-end avec PHP, incluant la gestion de bases de données avec MySQL et l'utilisation de Twig pour la templatisation.",
+  },
+  {
+    id: nanoid(),
+    title: "Stratégie de communication & Gestion de projet",
+    icon: <MdCampaign className="skill-icon" />,
+    text: "Compétences en communication digitale, gestion de projet en méthodologie Agile, et optimisation SEO pour améliorer la visibilité en ligne.",
+  },
+  {
+    id: nanoid(),
+    title: "Création numérique",
+    icon: <SiAdobeillustrator className="skill-icon" />,
+    text: "Maîtrise de la suite Adobe (Photoshop, Illustrator, InDesign) pour la conception graphique et la production de contenus numériques impactants.",
+  },
+  {
+    id: nanoid(),
+    title: "UI/UX Design",
+    icon: <FaFigma className="skill-icon" />,
+    text: "Utilisation avancée de Figma pour concevoir des interfaces intuitives, wireframes et prototypes interactifs.",
   },
 ];
 
 export const projects = [
   {
     id: nanoid(),
-    img: cocktail,
-    url: "https://verdant-syrniki-e21f07.netlify.app/",
-    github: "https://github.com/anjakely18/CocktailVerse",
-    title: "Cocktail Verse",
-    text: "Application web développée avec React qui permet de rechercher différentes variantes de cocktails.L'application utilise React Router pour une navigation fluide entre les résultats de recherche et les détails des recettes, récupérés via l'API TheCocktailDB. Les requêtes API sont effectuées avec Axios pour un traitement asynchrone et efficace des données.",
+    img: cuej,
+    url: "https://www.cuej.info/mini-sites/ruraux2024/index.php?",
+    github: "https://github.com/anjakely18/SITE-CUEJ",
+    title: "Site web pour le CUEJ - Ruraux 2024",
+    text: "Projet réalisé pour le CUEJ, permettant aux étudiants en journalisme de publier des reportages sur la vie rurale en 2024. Développé en PHP et Twig, avec une base de données MySQL pour gérer les articles et médias. J’ai principalement contribué au développement back-end et front-end, assurant une structure dynamique et une navigation fluide.",
   },
   {
     id: nanoid(),
-    img: toDo,
-    url: "https://bucolic-pika-937976.netlify.app/",
-    github: "https://github.com/anjakely18/to-do-list",
-    title: "To-do list",
-    text: "Application de to-do développée avec React. Cette application permet aux utilisateurs d'ajouter et de supprimer des taches, utilisant `useState` pour gérer l'état et `localStorage` pour persister les données.",
-  },
-  {
-    id: nanoid(),
-    img: simon,
-    url: "https://simon-game-anja.netlify.app/",
-    github: "https://github.com/anjakely18/javascript-the-simon-game",
-    title: "simon game",
-    text: "Jeu Simon, développé en JavaScript. Le jeu teste la mémoire des joueurs en leur demandant de reproduire des séquences de couleurs croissantes, avec des animations et des mécanismes de validation.",
-  },
-  {
-    id: nanoid(),
-    img: blog,
-    url: "http://visionnaire-life.atwebpages.com/",
-    github: "https://github.com/anjakely18/Blog-Personnel",
-    title: "Blog dynamique en php",
-    text: "Ce projet est un blog développé en PHP avec AJAX pour des interactions fluides sans rechargement. Il utilise une base de données pour gérer les articles, commentaires, catégories, et une barre de recherche. Le tout est construit en pur HTML et CSS pour un design simple et responsive.",
+    img: sobriete,
+    url: "https://rasamoelina.etu.mmi-unistra.fr/SAE303-site/",
+    github: "https://github.com/anjakely18/site-sobri-t-",
+    title: "Site de sensibilisation à la sobriété numérique",
+    text: "Projet visant à informer sur la sobriété numérique et ses enjeux environnementaux. J’ai conçu l’intégralité du design du site avec Figma et réalisé les illustrations. L’équipe a intégré des infographies dynamiques avec Chart.js, et j’ai veillé à une mise en page optimisée dans une démarche d’éco-conception web.",
   },
 ];
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,17 +8,21 @@ const About = () => {
       <SectionTitle title="Tout sur moi" />
       <div className="about-content">
         <p>
-          Je m'appelle Anja. Je suis une étudiante en deuxième année de BUT
-          Métiers du Multimédia et de l'Internet (MMI) avec une passion pour le
-          développement web. Après deux années en licence informatique, j'ai
-          acquis une solide expérience en programmation avec des compétences en
-          HTML, CSS, JavaScript, React, et PHP. Je me spécialise principalement
-          dans le développement front-end, où j'aime créer des interfaces
-          interactives et esthétiques qui enrichissent l'expérience utilisateur.
-          Mon intérêt pour le design et les aspects dynamiques du web me pousse
-          à concevoir des solutions à la fois innovantes et fonctionnelles.
-          Merci de visiter mon portfolio ! Je suis ouverte à toute opportunité
-          ou collaboration.
+          Je m'appelle Anja, étudiante en deuxième année de BUT Métiers du
+          Multimédia et de l'Internet (MMI), avec une spécialisation en
+          développement web. Mon parcours inclut également une expérience en
+          communication et en création numérique, que j'intègre dans mes projets
+          pour allier technique et créativité.<br></br> Passionnée par le
+          développement front et back, j'aime concevoir des interfaces
+          interactives et esthétiques en utilisant React, JavaScript, HTML, et
+          CSS. J'ai aussi des bases solides en PHP et en gestion de CMS. En
+          parallèle, je m'intéresse à l'UI/UX Design, au motion design avec
+          After Effects, et à la stratégie de communication digitale. <br></br>
+          J’ai travaillé sur divers projets, notamment la refonte de sites web,
+          des infographies dynamiques et des plateformes interactives. Je suis
+          toujours en quête de nouvelles opportunités et collaborations pour
+          continuer à apprendre et créer des expériences numériques innovantes.
+          Merci de visiter mon portfolio !
         </p>
         <Skills />
       </div>
