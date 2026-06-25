c241e2869780b1aed62c48ae63b24f5e6bb4d196
Anja <anja@studiometa.fr>
2026-06-25 09:52:57 +0200

    Modifie le style des icônes du Footer pour correspondre au Hero

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,16 @@ import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
     <section className="flex flex-col items-center container py-12 mt-20 border-t border-main/20">
-      <div className="flex justify-center items-center gap-4 mb-6">
+      <div className="flex justify-center items-center gap-4 mb-6 text-3xl">
         <a
           href="https://www.linkedin.com/in/anja-rasamoelina-59a8352a8/"
           target="_blank"
+          className="border border-main rounded-full p-2 transition-all duration-300 hover:bg-main hover:text-bg"
         >
-          <FaLinkedinIn className="" />
+          <FaLinkedinIn />
         </a>
-        <a href="https://github.com/anjakely18" target="_blank">
-          <FaGithub className="" />
+        <a href="https://github.com/anjakely18" target="_blank" className="border border-main rounded-full p-2 transition-all duration-300 hover:bg-main hover:text-bg">
+          <FaGithub />
         </a>
       </div>
       <nav className="text-nav flex flex-col md:flex-row justify-center items-center gap-3 md:gap-6 mb-6">
